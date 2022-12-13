ed5a7a33885f9b29332f8343c5946bf14bda1b3f
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-12 18:37:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT instructor_id, instructor_firstname from instructor";
+$sql = "SELECT instructor_firstname from instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
