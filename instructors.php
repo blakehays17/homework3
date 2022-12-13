606a37bc3fd486b4d0e63a5123ef54ecb9fb0125
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-13 12:37:11 -0600

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -1,54 +1,66 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("bootstrap.php"); ?>
+    <link rel="stylesheet" href="main.css">
     <title>Instructors</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Instructors</h1>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>First Name</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-$servername = "localhost";
-$username = "blakehay_s";
-$password = "wiZ#HZ^1]CAV";
-$dbname = "blakehay_sdb";
+</head>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+<body>
+    <?php include("header.php"); ?>
+    <div class="content">
+        <h1>Instructors</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Office</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $servername = "localhost";
+                    $username = "blakehay_s";
+                    $password = "wiZ#HZ^1]CAV";
+                    $dbname = "blakehay_sdb";
 
-$sql = "SELECT instructor_firstname, instructor_id from instructor";
-$result = $conn->query($sql);
+                    // Create connection
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    // Check connection
+                    if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                    }
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["instructor_id"]?></td>
-    <td><?=$row["instructor_firstname"]?></td>
-  </tr>
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
-  </tbody>
-    </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+                    $sql = "SELECT instructor_firstname, instructor_id, instructor_lastname, instructor_office from instructor";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                    ?>
+                    <tr>
+                        <td><?=$row["instructor_id"]?></td>
+                        <td><?=$row["instructor_firstname"]?></td>
+                        <td><?=$row["instructor_lastname"]?></td>
+                        <td><?=$row["instructor_office"]?></td>
+                    </tr>
+                    <?php
+                    }
+                    } else {
+                    echo "0 results";
+                    }
+                    $conn->close();
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <?php include("footer.php"); ?>
+</body>
+
 </html>
