9415cb8d026f357b8cbd4d255be6f340e8a3337e
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-13 18:44:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,31 @@
     <?php include("header.php"); ?>
     <div class="content">
         <h1>Homework 3</h1>
+        <br>
+        <h2>Instructors</h2>
+        <p>
+            On the instructors page you can view each instructor ID, first name, last name, and office. Lastly, you can
+            view which courses a given instructor teaches by clicking the "Courses" button. This is where the POST
+            variable is used.
+        </p>
+        <br>
+        <h2>Courses</h2>
+        <p>
+            On the courses page you can view each course ID, subject, course number, and course title.
+        </p>
+        <br>
+        <h2>Students</h2>
+        <p>
+            The students page consists of the student ID, name, age, and grade level. When clicking on a student's ID,
+            you will be able to view which courses that student is enrolled in. This is where a hyperlink is used to
+            filter data.
+        </p>
+        <br>
+        <h2>Enrollment</h2>
+        <p>
+            The enrollment page shows a a students enrollment in a certain course. You can view enrollment ID, student
+            name, and course title.
+        </p>
     </div>
     <?php include("footer.php"); ?>
 </body>
